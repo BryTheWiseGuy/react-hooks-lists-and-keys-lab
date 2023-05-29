0315d45d5d026e810146fba477449f2dee56d10b
Brian Ashman <brian.ashman@comcast.net>
2023-05-28 17:52:27 -0700

    Adjusted code in NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ function NavBar() {
   const links = ["home", "about", "projects"];
   
   return <nav>
-        {links.map((link) => {
-          <a key={link} href={`#${link}`}>{link}</a>
-        })}
+        <a key={links[0]} href={`#${links[0]}`}>{links[0]}</a>
+        <a key={links[1]} href={`#${links[1]}`}>{links[1]}</a>
+        <a key={links[2]} href={`#${links[2]}`}>{links[2]}</a>
         </nav>;
 }
 
